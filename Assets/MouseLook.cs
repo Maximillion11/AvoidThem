63d5e990ac4ba348d71e9ec43f3bdc039268ecbb
Maximillion11 <42713296+Maximillion11@users.noreply.github.com>
2020-09-25 17:26:57 +0100

    Initial commit

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Vector2 Sensitivity = new Vector2(1f, 1f);
+    public float rotationCapY = 80f;
+
+    private float rotationCountY = 0f;
+
+    public void Update()
+    {
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        Vector2 playerOutput = new Vector2(0, mouseInput.x * Sensitivity.x);
+        Vector2 cameraOutput = new Vector2(-mouseInput.y * Sensitivity.y, 0);
+
+        transform.Rotate(playerOutput);
+
+        if ((rotationCountY < rotationCapY || cameraOutput.x > 0) && (rotationCountY > -rotationCapY || cameraOutput.x < 0))
+        {
+            float distanceToRotationCapY = rotationCapY - Mathf.Abs(rotationCountY);
+            if (distanceToRotationCapY < Mathf.Abs(cameraOutput.x) && distanceToRotationCapY != 0)
+            {
+                cameraOutput.x = distanceToRotationCapY * Mathf.Sign(cameraOutput.x);
+            }
+
+            rotationCountY -= cameraOutput.x;
+            Camera.main.transform.Rotate(cameraOutput);
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float WalkSpeed = 3f;
+    public float SideStepSpeed = 2f;
+    public float SprintSpeed = 6f;
+    public float AccelerationSpeed = 2f;
+    public float DeAccelerationSpeed = 4f;
+    private Vector3 currentMoveSpeed;
+
+    private CharacterController cc;
+    public float Stamina = 5f;
+    public float StaminaRegenDelay = 2f;
+    private float currentStamina;
+    private float currentStaminaRegenDelay;
+    
+
+    private bool sprinting = false;
+
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+
+        currentStamina = Stamina;
+        currentStaminaRegenDelay = Stamina;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Sprint"))
+            sprinting = true;
+        else
+            sprinting = false;
+
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Move(moveInput);
+    }
+
+    private void Move(Vector3 moveInput)
+    {
+        Vector3 targetSpeed = Vector3.zero;
+        Vector3 movement = Vector3.zero;
+
+        if (moveInput.x != 0)
+        {
+            targetSpeed.x = moveInput.x * SideStepSpeed;
+            currentMoveSpeed.x = Mathf.Lerp(currentMoveSpeed.x, targetSpeed.x, AccelerationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            currentMoveSpeed.x = Mathf.Lerp(currentMoveSpeed.x, targetSpeed.x, DeAccelerationSpeed * Time.deltaTime);
+        }
+
+        if (moveInput.z != 0)
+        {
+            targetSpeed.z = moveInput.z * WalkSpeed;
+            if (sprinting && moveInput.z > 0 && currentStamina > 0)
+            {
+                targetSpeed.z = moveInput.z * SprintSpeed;
+
+                currentStamina -= Time.deltaTime;
+                currentStaminaRegenDelay = 0;
+            }
+
+            currentMoveSpeed.z = Mathf.Lerp(currentMoveSpeed.z, targetSpeed.z, AccelerationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            currentMoveSpeed.z = Mathf.Lerp(currentMoveSpeed.z, targetSpeed.z, DeAccelerationSpeed * Time.deltaTime);
+        }
+
+        movement.x = currentMoveSpeed.x;
+        movement.z = currentMoveSpeed.z;
+
+        movement = transform.TransformDirection(movement);
+
+        cc.SimpleMove(movement);
+
+        if (currentStaminaRegenDelay >= StaminaRegenDelay)
+        {
+            currentStaminaRegenDelay = 2;
+            if (!sprinting || moveInput.z <= 0)
+            {
+                if (currentStamina < Stamina)
+                    currentStamina += Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (!sprinting || moveInput.z <= 0)
+                currentStaminaRegenDelay += Time.deltaTime;
+        }
+
+        currentStamina = Mathf.Clamp(currentStamina, 0, Stamina);
+        currentStaminaRegenDelay = Mathf.Clamp(currentStaminaRegenDelay, 0, StaminaRegenDelay);
+    }
+}
